df3bc0b397041652f4300e84032446cc7b94391c
David Leon Gil <coruus@gmail.com>
2014-09-30 14:10:04 -0400

    Added print-impl.h, an objdump, and fixed up the shell script.

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-yasm rijn_mini.s -f macho64 && clang -std=c11 -Wextra mini.c rijn_mini.o /Users/dlg/repos/googlesource.com/boringssl/build/crypto/libcrypto.a -o mini.out && ./mini.out
+yasm rijn_mini.s -f macho64 && clang -std=c11 -Wextra mini.c rijn_mini.o libcrypto.a -o mini.out && ./mini.out
